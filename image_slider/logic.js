292287d6a3a9b074e3f3a5301ae13fadf3a243b1
ahmedabdelrahim123 <aahmed.mabdelrahim60@gmail.com>
2023-02-18 13:19:45 +0200

    task1 done

--- image_slider/logic.js
+++ image_slider/logic.js
@@ -1,15 +1,15 @@
-const swiper = new Swiper('.swiper', {
-    loop: false,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+var imgDuration = 3000;
+    var fadeSpeed = 2000;
+    var container = $('#slideshow-container');
+    var curIndex = -1;
 
-  });
+    function slideShow() {
+        container + $('img.slides').eq(curIndex).fadeOut(fadeSpeed);
+        container + $('img.slides').eq(curIndex  = curIndex < container.children().length - 1 ? curIndex + 1 : 0).fadeIn(fadeSpeed);
+     timeout= setTimeout("slideShow()", imgDuration);
+    }
+
+    slideShow();
+    $(".stop").click(function () {
+        clearTimeout(timeout);
+      });
---
+++ task2/logic.js
@@ -0,0 +1,11 @@
+$(".img1").click(function(){
+ $(".p1").toggleClass("hide");   
+});
+
+$(".img2").click(function(){
+  $(".p2").toggleClass("hide")
+});
+
+$(".img3").click(function(){
+    $(".p3").toggleClass("hide")
+  });
